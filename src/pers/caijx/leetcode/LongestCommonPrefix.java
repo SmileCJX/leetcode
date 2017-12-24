05f2ca8b3a819b51a440995fc9f79e44a2649c6c
SmileCJX <1092925421@qq.com>
2017-12-24 16:09:37 +0800

    最长前缀 by 分治算法 二分查找

--- src/pers/caijx/leetcode/LongestCommonPrefix.java
+++ src/pers/caijx/leetcode/LongestCommonPrefix.java
@@ -19,7 +19,7 @@ public class LongestCommonPrefix {
         for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0,prefix.length()-1);
-                while (prefix.isEmpty()) {
+                if (prefix.isEmpty()) {
                     return "";
                 }
             }
@@ -51,8 +51,78 @@ public class LongestCommonPrefix {
         return strs[0];
     }
 
+    /**
+     * 分治算法
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefix3(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        return longestCommonPrefix3(strs,0,strs.length-1);
+    }
+
+    private static String longestCommonPrefix3(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String lcpLeft = longestCommonPrefix3(strs,l,mid);
+            String lcpRight = longestCommonPrefix3(strs,mid+1,r);
+            return commonPrefit(lcpLeft,lcpRight);
+        }
+    }
+
+    private static String commonPrefit(String lcpLeft, String lcpRight) {
+        int min = Math.min(lcpLeft.length(),lcpRight.length());
+        for (int i = 0; i < min; i++) {
+            if (lcpLeft.charAt(i) != lcpRight.charAt(i)) {
+                return lcpLeft.substring(0,i);
+            }
+        }
+        return lcpLeft.substring(0,min);
+    }
+
+    /**
+     * 二分查找
+     * @param strs
+     * @return
+     */
+    public static String longestCommonPrefix4(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for (String str : strs) {
+            minLen = Math.min(minLen,str.length());
+        }
+        int low = 1;
+        int high = minLen;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (isCommonPrefit(strs,mid)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return strs[0].substring(0,(low + high) / 2);
+    }
+
+    private static boolean isCommonPrefit(String[] strs, int mid) {
+        String str1 = strs[0].substring(0,mid);
+        for (int i = 1; i < strs.length; i++) {
+            if (!strs[i].startsWith(str1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         String[] strs = new String[]{"leets","leetcode","leetkobeleet"};
-        System.out.println(longestCommonPrefix2(strs));
+        System.out.println(longestCommonPrefix4(strs));
     }
 }
