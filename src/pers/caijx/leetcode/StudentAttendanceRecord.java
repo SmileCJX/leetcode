05f0f42e205430e83726b17f4c20824ae774cca8
SmileCJX <1092925421@qq.com>
2018-01-14 12:19:36 +0800

    学生出勤记录

---
+++ src/pers/caijx/leetcode/StudentAttendanceRecord.java
@@ -0,0 +1,23 @@
+package pers.caijx.leetcode;
+
+/**
+ You are given a string representing an attendance record for a student.
+ The record only contains the following three characters:
+ 'A' : Absent.
+ 'L' : Late.
+ 'P' : Present.
+ A student could be rewarded if his attendance record doesn't contain more than one 'A' (absent) or more than two continuous 'L' (late).
+
+ You need to return whether the student could be rewarded according to his attendance record.
+ * Created by Administrator on 2018/1/14/014.
+ */
+public class StudentAttendanceRecord {
+
+    public static boolean checkRecord(String s) {
+        return !s.matches(".*LLL.*|.*A.*A.*");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkRecord("PPALLL"));
+    }
+}
