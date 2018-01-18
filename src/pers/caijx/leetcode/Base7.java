89d961185b4f415abe3e07f8cfcfc1cfefce4653
SmileCJX <1092925421@qq.com>
2018-01-18 23:36:20 +0800

    七进制

---
+++ src/pers/caijx/leetcode/Base7.java
@@ -0,0 +1,30 @@
+package pers.caijx.leetcode;
+
+/**
+ Given an integer, return its base 7 string representation.
+
+ Example 1:
+ Input: 100
+ Output: "202"
+ Example 2:
+ Input: -7
+ Output: "-10"
+ Note: The input will be in range of [-1e7, 1e7].
+ * Created by Administrator on 2018/1/18/018.
+ */
+public class Base7 {
+
+    public static String convertToBase7(int num) {
+        if (num < 0) {
+            return '-' + convertToBase7(-num);
+        }
+        if (num < 7) {
+            return num + "";
+        }
+        return convertToBase7(num / 7) + num % 7;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convertToBase7(20));
+    }
+}
