dbcd1580eeca6a27f1e4d804f8d96c2cf7e32fbd
SmileCJX <1092925421@qq.com>
2018-02-13 22:34:41 +0800

    删除链表的节点

---
+++ src/pers/caijx/leetcode/DeleteNodeInALinkedList.java
@@ -0,0 +1,20 @@
+package pers.caijx.leetcode;
+
+/**
+ Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+ he linked list should become 1 -> 2 -> 4 after calling your function.
+ * Created by Administrator on 2018/2/13/013.
+ */
+public class DeleteNodeInALinkedList {
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
