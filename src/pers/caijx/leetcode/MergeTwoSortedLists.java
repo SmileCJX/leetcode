031220426f73fc2a9898e438dce9d4c737d85ee7
SmileCJX <1092925421@qq.com>
2018-03-25 10:46:02 +0800

    合并有序列表

---
+++ src/pers/caijx/leetcode/MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+package pers.caijx.leetcode;
+
+/**
+ Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+ Example:
+
+ Input: 1->2->4, 1->3->4
+ Output: 1->1->2->3->4->4
+
+ * Created by Administrator on 2018/3/25/025.
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next,l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+}
