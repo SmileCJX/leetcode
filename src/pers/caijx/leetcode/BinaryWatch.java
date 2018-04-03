10250b42ac4cf04b5145e87bbd5c9bcaf2b1bbde
SmileCJX <1092925421@qq.com>
2018-04-03 22:21:11 +0800

    二进制手表

---
+++ src/pers/caijx/leetcode/BinaryWatch.java
@@ -0,0 +1,35 @@
+package pers.caijx.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ A binary watch has 4 LEDs on the top which represent the hours (0-11), and the 6 LEDs on the bottom represent the minutes (0-59).
+
+ Each LED represents a zero or one, with the least significant bit on the right.
+
+
+ For example, the above binary watch reads "3:25".
+
+ Given a non-negative integer n which represents the number of LEDs that are currently on, return all possible times the watch could represent.
+
+ Example:
+
+ Input: n = 1
+ Return: ["1:00", "2:00", "4:00", "8:00", "0:01", "0:02", "0:04", "0:08", "0:16", "0:32"]
+ * Created by Administrator on 2018/4/3/003.
+ */
+public class BinaryWatch {
+
+    public List<String> readBinaryWatch(int num) {
+        List<String> times = new ArrayList<>();
+        for (int h = 0; h < 12; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (Integer.bitCount(h * 64 + m) == num) {
+                    times.add(String.format("%d:%02d",h,m));
+                }
+            }
+        }
+        return times;
+    }
+}
