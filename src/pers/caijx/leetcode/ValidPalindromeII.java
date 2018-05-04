22c69203fb3f4215e8835c0b24049ca6d5491c0a
SmileCJX <1092925421@qq.com>
2018-05-04 16:47:21 +0800

    校验回文

---
+++ src/pers/caijx/leetcode/ValidPalindromeII.java
@@ -0,0 +1,46 @@
+package pers.caijx.leetcode;
+
+/**
+ Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+ Example 1:
+ Input: "aba"
+ Output: True
+ Example 2:
+ Input: "abca"
+ Output: True
+ Explanation: You could delete the character 'c'.
+ Note:
+ The string will only contain lowercase characters a-z. The maximum length of the string is 50000.
+ * Created by Administrator on 2018/5/4/004.
+ */
+public class ValidPalindromeII {
+
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j && s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+        }
+        if (i >= j) {
+            return true;
+        }
+        if (isPlain(s,i + 1,j) || isPlain(s,i,j - 1)) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isPlain(String s,int i,int j) {
+        while (i < j) {
+            if (s.charAt(i) == s.charAt(j)) {
+                i++;
+                j--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
