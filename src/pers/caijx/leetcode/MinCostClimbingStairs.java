7c668c9358f889ad9424a1c5aeac86cd1e716dc0
SmileCJX <1092925421@qq.com>
2018-05-11 23:26:24 +0800

    攀登楼梯

--- src/pers/caijx/leetcode/MinCostClimbingStairs.java
+++ src/pers/caijx/leetcode/MinCostClimbingStairs.java
@@ -41,6 +41,14 @@ public class MinCostClimbingStairs {
         return dp2;
     }
 
+    /**
+     用一个数组dp[]来存放到达每一层所需要的花费值。则则最终结果是求dp[cost.length]的值。因为每次可以走1层或者2层，
+     并且可以从0或者从1开始，所以可以得到dp[0]为0，dp[1]为0。从2开始，dp[i]可以由通过dp[i-2]走2层或者通过dp[i-1]走一层到达，
+     而这i-2和i-1层所要花费的值分别为cost[i-2]和cost[i-1]，
+     所以，dp[i] = min(dp[i-2] + cost[i-2], dp[i-1] + cost[i-1])。该算法的空间复杂度为O(n)，时间复杂度为O(n)。
+     * @param cost
+     * @return
+     */
     public int minCostClimbingStairs2(int[] cost) {
         int length = cost.length + 1;
         int[] dp = new int[length];
