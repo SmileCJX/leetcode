4649aefe70a05bfd0b9adbc35de5bb9f518e8f48
SmileCJX <1092925421@qq.com>
2018-05-13 10:35:40 +0800

    字母大小写置换

--- src/pers/caijx/leetcode/LetterCasePermutation.java
+++ src/pers/caijx/leetcode/LetterCasePermutation.java
@@ -26,6 +26,26 @@ public class LetterCasePermutation {
 
    public List<String> res;
 
+    /**
+     Explaination
+
+     if a char is a digit, we only need to do one recursion, and there is no need for toggling between lower and upper cases.
+
+     if a char is a letter, we need to do recursion for both lower and upper cases, since we do not know whether that character is lower or upper at the beginning, we just simply do both separately.
+     if we reach the end, we simply add it to the result list
+
+     The reason for using a LinkedList instead of an ArrayList is we are always do insertion for this problem. Since ArrayList automatically resizes itself, using LinkedList is more efficient for insertion.
+
+     Time complexity : O(2^n * n)
+
+     Since in the worst case, all characters are letters. For each of them, it do both upper and lower case. And there will be 2*n strings. Also, for each string, it uses a String constructor, new String(ca), which implemented with Arrays.copyOf, that is O(n). n is the length of String.
+
+     Space complexity: O(2^n * n)
+
+     In the worst case, the total memory needed is O(2^n * n) , including a trivial extra space for holding the answer, a LinkedList.
+     * @param s
+     * @return
+     */
     public List<String> letterCasePermutation(String s) {
         res = new LinkedList<>();
         helper(s.toCharArray(),0);
